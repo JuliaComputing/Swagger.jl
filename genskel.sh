c4fbdc2068bae1c4e6e5133506a497a9dd66eb01
tan <tanmaykm@gmail.com>
2016-10-23 13:50:14 +0530

    correct julia version in REQUIRE

--- genskel.sh
+++ genskel.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 
+# Generate skeleton code for Julia Swagger code generator
+# Do not run this unless upgrading to a Swagger version/interface
 java -jar modules/swagger-codegen-cli/target/swagger-codegen-cli.jar meta -o ./plugin -n julia -p com.juliacomputing.swagger.codegen
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,9 @@
 using Swagger
 using Base.Test
 
+const gencmd = joinpath(dirname(@__FILE__()), "petstore", "generate.sh")
+println("Generating petstore using $gencmd")
+run(`$gencmd`)
+
+println("Running petstore tests...")
 include("petstore/runtests.jl")
