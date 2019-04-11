adeae0c59bc8e9f8f678224272b2703222991c23
tan <tanmaykm@gmail.com>
2019-04-11 10:09:16 +0530

    remove pre-Julia1.0 code

--- src/Swagger.jl
+++ src/Swagger.jl
@@ -7,12 +7,7 @@ using Dates
 
 import Base: convert, show, summary, getindex, keys, length
 import JSON: lower
-
-@static if VERSION < v"0.7.0-"
-    import Base: start, done, next
-else
-    import Base: iterate
-end
+import Base: iterate
 
 abstract type SwaggerModel end
 abstract type SwaggerApi end
