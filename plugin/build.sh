d4c642911a65b44809140c34bd86fe7ff907eb20
Tanmay Mohapatra <tanmaykm@gmail.com>
2019-09-12 13:06:09 +0530

    update .travis.yml, more robust build
    - fix build errors due to Pkg differences
    - simplify build by not building swagger from source
    - test against both oraclejdk and openjdk
    - add more Julia versions to CI

--- plugin/build.sh
+++ plugin/build.sh
@@ -2,31 +2,14 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PKG_DIR=`readlink -e ${DIR}/..`
-source ${DIR}/ver.sh
-
-BUILD_DIR=${PKG_DIR}/swagger-codegen-${VER_CODEGEN}
-LIBSRC=${BUILD_DIR}/modules/swagger-codegen-cli/target/lib
-LIBDST=${PKG_DIR}/plugin/lib/io/swagger
-MODELS=${LIBDST}/swagger-models/${VER_MODELS}/swagger-models-${VER_MODELS}.jar
-CODEGEN=${LIBDST}/swagger-codegen/${VER_CODEGEN}/swagger-codegen-${VER_CODEGEN}.jar
-
-if [ ! -d "${BUILD_DIR}" ] || [ ! -f "${MODELS}" ] || [ ! -f "${CODEGEN}" ]
-then
-    echo "Cleaning stale folders..."
-    rm -rf "${BUILD_DIR}"
-    echo "Downloading swagger-codegen..."
-    curl -s -L https://github.com/swagger-api/swagger-codegen/archive/v${VER_CODEGEN}.tar.gz | tar -C "${PKG_DIR}" -x -z -f -
-    echo "Building swagger-codegen..."
-    cd "${BUILD_DIR}" && mvn clean package
-fi
-
-mkdir -p ${LIBDST}/swagger-models/${VER_MODELS}
-mkdir -p ${LIBDST}/swagger-codegen/${VER_CODEGEN}
-
-[ ! -f "${MODELS}" ] && echo "Copying swagger-models jar..." && cp ${LIBSRC}/swagger-models-${VER_MODELS}.jar ${MODELS}
-[ ! -f "${CODEGEN}" ] && echo "Copying swagger-codegen jar..." && cp ${LIBSRC}/swagger-codegen-${VER_CODEGEN}.jar ${CODEGEN}
-
 cd ${PKG_DIR}/plugin
 echo "Building Julia plugin..."
 mvn package
+mvn dependency:resolve dependency:build-classpath -Dmdep.outputFile=classpath.tmp
+echo "`cat classpath.tmp`:$DIR/target/julia-swagger-codegen-0.0.2.jar" > classpath
+rm -f ./classpath.tmp
+echo "Build successful"
+echo "---------------------------------------"
+echo "CLASSPATH=`cat classpath`"
+echo "---------------------------------------"
 echo "Done"
